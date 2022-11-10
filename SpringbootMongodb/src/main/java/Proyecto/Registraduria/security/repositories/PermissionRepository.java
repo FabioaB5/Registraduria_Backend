7cc3fcafc9b369a2bcdb315fb40743becb08ec5e
FabioaB5 <andresbarreiroa@gmail.com>
2022-11-10 10:35:40 -0500

    Actualizacion

--- SpringbootMongodb/src/main/java/Proyecto/Registraduria/security/repositories/PermissionRepository.java
+++
@@ -1,8 +0,0 @@
-package Proyecto.Registraduria.security.repositories;
-
-import Proyecto.Registraduria.security.models.Permission;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface PermissionRepository
-        extends MongoRepository<Permission, String> {
-}
--- SpringbootMongodb/src/main/java/Proyecto/Registraduria/security/repositories/RoleRepository.java
+++
@@ -1,8 +0,0 @@
-package Proyecto.Registraduria.security.repositories;
-
-import Proyecto.Registraduria.security.models.Role;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface RoleRepository
-        extends MongoRepository<Role, String> {
-}
